6706970001431af640a9b517961eb45256dcf529
Stephen Rimbakusumo <srimbakusumo@gmail.com>
2016-07-15 10:32:28 -0700

    Added Bootstrap CSS

--- src/main.js
+++ src/main.js
@@ -1,8 +1,13 @@
-// main.js
+var jquery = require('jquery');
+
+var bootstrap = require('bootstrap-webpack');
+
 var React = require('react');
 var ReactDOM = require('react-dom');
 
+var reactBootstrap = require('react-bootstrap');
+
 ReactDOM.render(
-    <h1>Hello Test dWorsdl!</h1>,
+    <h1>Hello, world!</h1>,
     document.getElementById('example')
 );
--- webpack.config.js
+++ webpack.config.js
@@ -10,7 +10,6 @@ module.exports = {
         path: path.resolve(__dirname, "public/shared"),
         filename: 'compiled.js'
     },
-    devtool: 'source-map',
     module: {
         loaders: [
             {
@@ -29,7 +28,17 @@ module.exports = {
                     presets: ['es2015', 'react'],
                 }
             },
+            { test: /\.css$/, loader: 'style-loader!css-loader' },
+            { test: /\.eot(\?v=\d+\.\d+\.\d+)?$/, loader: "file" },
+            { test: /\.(woff|woff2)$/, loader:"url?prefix=font/&limit=5000" },
+            { test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/, loader: "url?limit=10000&mimetype=application/octet-stream" },
+            { test: /\.svg(\?v=\d+\.\d+\.\d+)?$/, loader: "url?limit=10000&mimetype=image/svg+xml" }
         ]
+    },
+    resolve: {
+        alias: {
+            jquery: "jquery/src/jquery"
+        }
     },
     debug: true
 };
